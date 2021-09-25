4b2653952ec0b53a7a458fb89c2cbcfb5ce63c04
lowieCOOL <lowie.vang@gmail.com>
2021-09-25 22:41:32 +0200

    Completed player movement. Added player rotation. Locked camera to player, but added rotation on x-axis. Refactored some code.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
-    [SerializeField] float MovementSpeed = 7.5f;
-    //[SerializeField] float RotationSpeed = 7.5f;
+    float mouseX, mouseY, xRotation = 0;
+    [SerializeField] float xRotationMinClamp = -70f;
+    [SerializeField] float xRotationMaxClamp = 70f;
+    [SerializeField] GameObject playerObject;
+    [SerializeField] GameObject cameraObject;
+    [SerializeField] float movementSpeed = 250f;
+    [SerializeField] float rotationSpeed = 7.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
 
-        rb = GetComponent<Rigidbody>();
+        rb = playerObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -27,28 +32,32 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Q))
         {
-            rb.AddRelativeForce(Vector3.left * MovementSpeed * Time.deltaTime);
+            rb.AddRelativeForce(Vector3.left * movementSpeed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rb.AddRelativeForce(Vector3.right * MovementSpeed * Time.deltaTime);
+            rb.AddRelativeForce(Vector3.right * movementSpeed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.Z))
         {
             Debug.Log("Forward");
-            rb.AddRelativeForce(Vector3.forward * MovementSpeed * Time.deltaTime);
+            rb.AddRelativeForce(Vector3.forward * movementSpeed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            rb.AddRelativeForce(Vector3.back * MovementSpeed * Time.deltaTime);
+            rb.AddRelativeForce(Vector3.back * movementSpeed * Time.deltaTime);
         }
     }
 
         void RotatePlayer()
     {
-        float xValue = Input.GetAxis("Mouse Y");
-        float yValue = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+        mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
 
-        transform.Rotate(0, yValue, 0);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, xRotationMinClamp, xRotationMaxClamp);
+
+        cameraObject.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(0, mouseX, 0);
     }
 }
