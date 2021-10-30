93fc4d7e2e7e7f1d1c19d67826ea733463712dc3
lowieCOOL <lowie.vang@gmail.com>
2021-10-30 21:11:58 +0200

    Added the glowing functionallity to the pedestal signs.

--- Assets/Scripts/SignHighlighter.cs
+++ Assets/Scripts/SignHighlighter.cs
@@ -7,6 +7,8 @@ public class SignHighlighter : MonoBehaviour
     Transform headTransform;
     RaycastHit hit;
     int layerMask = 1 << 8;
+    GameObject hitObject;
+    bool lastCastWasHit = false;
     [SerializeField] GameObject cameraObject;
     [SerializeField] float detectionRange = 2f;
     
@@ -23,12 +25,14 @@ public class SignHighlighter : MonoBehaviour
         if (Physics.Raycast(headTransform.position, headTransform.TransformDirection(Vector3.forward), out hit, detectionRange, layerMask))
         {
             Debug.DrawRay(headTransform.position, headTransform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-
+            hitObject = hit.collider.gameObject;
+            hitObject.GetComponent<SignGlowController>().ActivateSignGlow();
+            lastCastWasHit = true;
         }
         else
         {
             Debug.DrawRay(headTransform.position, headTransform.TransformDirection(Vector3.forward) * detectionRange, Color.white);
-
+            if (lastCastWasHit) { hitObject.GetComponent<SignGlowController>().DeActivateSignGlow(); }
         }
     }
 }
